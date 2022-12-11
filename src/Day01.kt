313ae94de82926a4d383891a750d2cec52871f01
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-12-11 12:51:55 +0100

    Day01_02

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,5 +15,22 @@ fun main() {
         return max(max, acc)
     }
 
+    fun part2(input: List<String>): Int {
+        var acc = 0
+        val all = mutableListOf<Int>()
+        for (s in input) {
+            if (s.isEmpty()) {
+                all.add(acc)
+                acc = 0
+            } else {
+                acc += s.toInt()
+            }
+        }
+        all.add(acc)
+        return all.sortedDescending().take(3).sum()
+    }
+
+
     part1(readInput("input")).println()
+    part2(readInput("input")).println()
 }
