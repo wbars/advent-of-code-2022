da622881102effa291c0bee266d2ccb371d2f65f
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-12-11 13:12:00 +0100

    Day02_02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,33 @@
-import kotlin.math.max
-
 fun main() {
     fun part1(input: List<String>): Int {
         return input.asSequence()
                 .map { Pair(it[0].toCode(), it[2].toCode()) }
                 .map { it.second + 1 + it.solve() }
                 .sum()
+    }
 
+    fun part2(input: List<String>): Int {
+        return input.asSequence()
+                .map {
+                    val first = it[0] - 'A'
+                    Pair(first, it[2].toCode(first))
+                }
+                .map { it.second + 1 + it.solve() }
+                .sum()
     }
 
 
+
     part1(readInput("input")).println()
+    part2(readInput("input")).println()
+}
+
+private fun Char.toCode(p: Int): Int {
+    return when (this) {
+        'X' -> (p - 1).let { if (it < 0) 2 else it  }
+        'Y' -> p
+        else -> (p + 1) % 3
+    }
 }
 
 private fun Char.toCode(): Int {
