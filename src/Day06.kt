5dc1db8780306edabf444658c38613dc0ca2f071
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-12-17 15:34:31 +0100

    Day06_2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,25 +1,17 @@
-
-
-private fun getStacks() = listOf(
-        ArrayDeque(listOf('R', 'Q', 'G', 'P', 'C', 'F')),
-        ArrayDeque(listOf('P', 'C', 'T', 'W')),
-        ArrayDeque(listOf('C', 'M', 'P', 'H', 'B')),
-        ArrayDeque(listOf('R', 'P', 'M', 'S', 'Q', 'T', 'L')),
-        ArrayDeque(listOf('N', 'G', 'V', 'Z', 'J', 'H', 'P')),
-        ArrayDeque(listOf('J', 'P', 'D')),
-        ArrayDeque(listOf('R', 'T', 'J', 'F', 'Z', 'P', 'G', 'L')),
-        ArrayDeque(listOf('J', 'T', 'P', 'F', 'C', 'H', 'L', 'N')),
-        ArrayDeque(listOf('W', 'C', 'T', 'H', 'Q', 'Z', 'V', 'G')),
-)
-
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun solve(input: List<String>, size: Int): Int {
         val line = input[0]
-        return (line.indices).find { line.substring(it, it + 4).toCharArray().toSet().size == 4 }!! + 4
+        return (line.indices).find { line.substring(it, it + size).toCharArray().toSet().size == size }!! + size
     }
 
+    fun part1(input: List<String>): Int {
+        return solve(input, 4)
+    }
 
-
+    fun part2(input: List<String>): Int {
+        return solve(input, 14)
+    }
 
     part1(readInput("input")).println()
+    part2(readInput("input")).println()
 }
