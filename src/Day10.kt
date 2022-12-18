90732b1f34965ec784bffef9c689a044730a2105
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-12-18 15:49:27 +0100

    Day10_2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,5 +1,3 @@
-import kotlin.math.abs
-
 fun main() {
 
     fun part1(input: List<String>): Int {
@@ -29,5 +27,37 @@ fun main() {
         return res
     }
 
+    fun part2(input: List<String>): Int {
+        val a: MutableList<Int> = mutableListOf()
+        var curCycle = 0
+        var x = 1
+        for (line in input) {
+            a.add(x)
+            if (line == "noop") {
+                curCycle++
+            } else {
+                a.add(x)
+                val v = line.split(" ")[1].toInt()
+                x += v
+                curCycle += 2
+            }
+        }
+
+        for (i in 1 until 241) {
+            if (((i - 1) % 40) in a[i - 1] - 1..a[i - 1] + 1) {
+                print("#")
+            } else {
+                print(".")
+            }
+            if (i % 40 == 0) {
+                println()
+            }
+
+        }
+        return a.size
+    }
+
+
     part1(readInput("input")).println()
+    part2(readInput("input"))
 }
